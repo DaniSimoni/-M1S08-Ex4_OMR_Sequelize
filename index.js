0650bf1818c817d1c1fde9693952de0194052269
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-03-25 16:48:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/* Crie uma tabela em um banco de dados com o OMR Sequelize e insira dados nessa tabela. */
+
+
+(async()=>{
+
+    const database = require('./db');
+    const Aluno = require('./aluno');
+    await database.sync();
+    
+
+    const novoAluno = await Aluno.create({
+
+        nome: 'Tom',
+        idade: '10',
+        curso: 'Gestão'
+    })
+
+})();
